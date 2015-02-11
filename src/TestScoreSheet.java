f2a14086f92425dfbf8c7ce599773d8082511f5a
John Hayman <jrhayman@uwm.edu>
2015-02-11 11:45:46 -0600

    finished scoresheet may need getters as test are created, created test for ScoreSheet, test for individual Frame may not be needed,

---
+++ src/TestScoreSheet.java
@@ -0,0 +1,36 @@
+import java.util.Comparator;
+import junit.framework.TestCase;
+
+public class TestScoreSheet extends TestCase{
+//	
+	private ScoreSheet ss1 = new ScoreSheet();
+//	
+	private ScoreSheet ss2 = new ScoreSheet();
+//	
+	private ScoreSheet ss3 = new ScoreSheet();
+	public void testOneThrow(){
+		ss1.addThrow(0);
+		ss2.addThrow(5);
+		ss3.addThrow(9);
+		
+		assertTrue(ss1.getFrame()==2);
+		assertTrue(ss2.getFrame()==1);
+		assertTrue(ss3.getFrame()==1);
+		
+		assertEquals(ss1.getScore(), 0);
+		assertEquals(ss2.getScore(), 0);
+		assertEquals(ss3.getScore(), 0);
+		
+		ss2.addThrow(0);
+		ss3.addThrow(1);
+		
+		assertTrue(ss2.getFrame()==2);
+		assertTrue(ss3.getFrame()==2);
+		
+		assertEquals(ss2.getScore(), 0);
+		assertEquals(ss3.getScore(), 9);
+		
+		
+	}
+	public void testTwoThrows
+}
